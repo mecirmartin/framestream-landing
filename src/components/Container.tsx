d9964fc2adec57d47d02cbd5ed2bfdc009972e29
mecirmartin <mecir.martin@gmail.com>
2024-11-23 17:59:07 +0100

    Fix padding

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -8,7 +8,7 @@ interface ContainerProps {
 export function Container(props: Readonly<ContainerProps>) {
   return (
     <div
-      className={`container py-8 px-6 sm:px-8 mx-auto xl:px-0 ${
+      className={`container py-8 px-4 sm:px-8 mx-auto xl:px-0 ${
         props.className ? props.className : ""
       }`}
     >
